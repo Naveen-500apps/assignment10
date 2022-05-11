2d221a86438b0321b0fe73835b4617fa9baaa48c
naveen@1141 <102018169+Naveen-500apps@users.noreply.github.com>
2022-05-11 16:05:42 +0530

    Add files via upload

---
+++ Que2.vue
@@ -0,0 +1,23 @@
+<template>
+  <div> 
+    <b-form-input v-model="lowercase" ></b-form-input
+    ><br/><br/>
+    <b-form-input v-model="uppercase"></b-form-input>
+  </div>
+</template>
+<script>
+export default {
+  name: "QuestIon2",
+  data() {
+    return {
+      lowercase: "",
+      uppercase: "",
+    };
+  },
+  watch: {
+    lowercase() {
+      this.uppercase = this.lowercase.toUpperCase();
+    },
+  },
+};
+</script>
